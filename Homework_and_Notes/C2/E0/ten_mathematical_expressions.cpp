d457bcd3d62608abf862c5b0b1a2e4d17d39e70f
AntoanNikolov <antoanameya@gmail.com>
2025-09-02 21:09:22 -0400

    Finished Exercise Set Zero

---
+++ Homework_and_Notes/C2/E0/ten_mathematical_expressions.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+    int m = 7;
+    cout << "The product of " << n << " and " << m << " is ";
+    cout << n * m << "." << endl;
+
+    int a = 6;
+    int b = 7;
+    cout << "The sum of " << a << " and " << b << " is ";
+    cout << a + b << "." << endl;
+
+    int c = 7;
+    int d = 8;
+    cout << "The difference of " << c << " and " << d << " is ";
+    cout << c - d << "." << endl;
+
+    int e = 9;
+    int f = 10;
+    cout << "The quotient of " << e << " and " << f << " is ";
+    cout << e - f << "." << endl;
+
+    int g = 11;
+    int h = 12;
+    cout << "The remainded after division of " << g << " and " << h << " is ";
+    cout << g % h << "." << endl;
+
+    int i = 50;
+    int j = 10;
+    cout << "The product of " << i << " and " << j << " is ";
+    cout << i * j << "." << endl;
+
+    int k = 11;
+    int l = 15;
+    cout << "The sum of " << k << " and " << l << " is ";
+    cout << k + l << "." << endl;
+
+    int o = 12;
+    int p = 2;
+    cout << "The quotient of " << o << " and " << p << " is ";
+    cout << o - p << "." << endl;
+
+    int q = 4;
+    int r = 2;
+    cout << "The remainded after division of " << q << " and " << r << " is ";
+    cout << q % r << "." << endl;
+
+    int y = 1;
+    int z = 9;
+    cout << "The difference of " << y << " and " << z << " is ";
+    cout << y - z << "." << endl;
+
+
+    
+    return 0;
+}
