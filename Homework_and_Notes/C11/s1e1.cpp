1417f36f8c0e1b997d17971eeef19a2be6e7f61c
AntoanNikolov <antoanameya@gmail.com>
2025-11-12 09:24:29 -0500

    started problem 1

--- Homework_and_Notes/C11/s1e1.cpp
+++ Homework_and_Notes/C11/s1e1.cpp
@@ -1,11 +1,27 @@
-define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest.h>
-#include "Time.h"
 using namespace std;
 
+struct Time {
+    int hour, minute;
+    double second;
 
+};
 
+Time::Time(double secs=0)
+{
+    hour = int(secs / 3600.0);
+    secs -= hour * 3600.0;
+    minute = int(secs / 60.0);
+    secs -= minute * 60;
+    second = secs;
+}
 
+void Time::to_string()
+{
+    timestring = hour+":"+minute+":"+second;
+    return(timestring);
+}
 
 TEST_CASE("Test can create and render Times") {
     Time t1;
