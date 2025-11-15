cbf44e361f5530e38326da7f87569501bd0cd04c
AntoanNikolov <antoanameya@gmail.com>
2025-11-15 18:52:41 -0500

    progressing s1e5

--- Homework_and_Notes/C11/s1e5.cpp
+++ Homework_and_Notes/C11/s1e5.cpp
@@ -2,8 +2,25 @@
 #include <doctest.h>
 #include <iostream>
 #include <string>
-#include "Point.h"
 using namespace std;
+
+
+struct Point {
+    double x, y;
+
+    Point();
+    Point p2(double a, double b);
+
+};
+
+Point::Point(double a, double b){
+    x=a;
+    y = b;
+}
+
+
+
+
 
 TEST_CASE("Test can create Points") {
     Point p1;
@@ -14,16 +31,16 @@ TEST_CASE("Test can create Points") {
     CHECK(p2.y == 4.0);
 }
 
-TEST_CASE("Test can add Points") {
-    Point p1(3, 4);
-    Point p2(5, 2);
-    Point p3 = p1 + p2;
-    CHECK(p3.x == 8.0);
-    CHECK(p3.y == 6.0);
-}
+// TEST_CASE("Test can add Points") {
+//     Point p1(3, 4);
+//    Point p2(5, 2);
+//    Point p3 = p1 + p2;
+//    CHECK(p3.x == 8.0);
+//    CHECK(p3.y == 6.0);
+// }
 
-TEST_CASE("Test can render Points as strings") {
-    Point p(8, 7);
-    string expected = "(8.000000, 7.000000)";
-    CHECK(p.to_string() == expected);
-}
+// TEST_CASE("Test can render Points as strings") {
+//    Point p(8, 7);
+//    string expected = "(8.000000, 7.000000)";
+//    CHECK(p.to_string() == expected);
+// }
