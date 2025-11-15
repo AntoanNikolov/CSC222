b5869d770e360b851fed23cc532ad879b5158007
AntoanNikolov <antoanameya@gmail.com>
2025-11-15 18:19:58 -0500

    started s1e4

--- Homework_and_Notes/C11/s1e4.cpp
+++ Homework_and_Notes/C11/s1e4.cpp
@@ -9,10 +9,10 @@ struct Time {
 
     Time(int secs = 0);
     Time(int hrs, int mins, int secs=0);
-    
+
     string to_string() const;
     Time operator+(const Time&);
-    
+
 
 };
 
@@ -53,26 +53,11 @@ Time Time::operator+(const Time& othertime) {
 
 }
 
+int main() {
+    Time t(3600 * 4 + 60 * 11 + 22);
+    cout << "Time t is: " << t << endl;
+    // should print 4:11:22
 
-TEST_CASE("Test can create and render Times") {
-    Time t1;
-    CHECK(t1.to_string() == "0:00:00");
-    Time t2(7);
-    CHECK(t2.to_string() == "0:00:07");
-    Time t3(72);
-    CHECK(t3.to_string() == "0:01:12");
-    Time t4(7 * 3600 + 11 * 60 + 19);
-    CHECK(t4.to_string() == "7:11:19");
+    return 0;
 }
-TEST_CASE("Test hour-minute and hour-minute-second constructors") {
-    Time t1(5, 37);
-    CHECK(t1.to_string() == "5:37:00");
-    Time t2(7, 2, 11);
-    CHECK(t2.to_string() == "7:02:11");
-}
-TEST_CASE("Test can add two Times with + operator") {
-    Time t1(25, 40);
-    Time t2(17, 2, 42);
-    Time t3 = t1 + t2;
-    CHECK(t3.to_string() == "42:42:42");
-}
+
