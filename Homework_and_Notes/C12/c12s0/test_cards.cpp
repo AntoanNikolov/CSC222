dba9f911ede72bb58c79bd80d31f23399253c9a2
AntoanNikolov <antoanameya@gmail.com>
2025-11-19 16:48:03 -0500

    starting c12s0e2

--- Homework_and_Notes/C12/c12s0/test_cards.cpp
+++ Homework_and_Notes/C12/c12s0/test_cards.cpp
@@ -14,3 +14,11 @@ TEST_CASE("Test can create Cards") {
     CHECK(c2.rank == 4);
 }
 
+TEST_CASE("Test can render Cards") {
+    Card c3(3, 12);
+    CHECK(c3.to_string() == "Queen of Hearts");
+    Card c4(2, 10);
+    CHECK(c4.to_string() == "10 of Diamonds");
+    Card c5;
+    CHECK(c5.to_string() == "Joker");
+}
