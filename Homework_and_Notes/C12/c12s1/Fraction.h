4a36d9cb86c1c7ac258a9cd6614fcb450b800db9
AntoanNikolov <antoanameya@gmail.com>
2025-11-22 17:43:38 -0500

    c12s1e2 passes

--- Homework_and_Notes/C12/c12s1/Fraction.h
+++ Homework_and_Notes/C12/c12s1/Fraction.h
@@ -1,8 +1,10 @@
+#include <string>
 struct Fraction
 {
     int numerator, denominator;
 
     Fraction();
     Fraction(int n,int d);
+    Fraction(std::string a);
 
 };
--- Homework_and_Notes/C12/c12s1/Fractions.cpp
+++ Homework_and_Notes/C12/c12s1/Fractions.cpp
@@ -1,5 +1,5 @@
 #include "Fraction.h"
-// using namespace std;
+// #include <string>
 
 Fraction::Fraction() {
     denominator = 1;
@@ -12,4 +12,10 @@ Fraction::Fraction() {
 Fraction::Fraction(int n,int d) {
     denominator = d;
     numerator = n;
+}
+
+Fraction::Fraction(std::string a) {
+    int slash = a.find('/');
+    numerator = std::stoi(a.substr(0, slash)); // stoi is more intuitive
+    denominator = std::stoi(a.substr(slash + 1));
 }
