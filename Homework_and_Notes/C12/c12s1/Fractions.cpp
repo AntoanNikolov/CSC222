3094d460811346f91e4c02ba695698b15b438955
AntoanNikolov <antoanameya@gmail.com>
2025-11-22 18:16:42 -0500

    c12s1e4 passes

--- Homework_and_Notes/C12/c12s1/Fractions.cpp
+++ Homework_and_Notes/C12/c12s1/Fractions.cpp
@@ -1,30 +1,6 @@
 #include "Fraction.h"
 // #include <string>
 
-Fraction::Fraction() {
-    denominator = 1;
-    numerator = 0;
-}
-
-
-
-
-Fraction::Fraction(int n,int d) {
-    denominator = d;
-    numerator = n;
-}
-
-Fraction::Fraction(std::string a) {
-    int slash = a.find('/');
-    numerator = std::stoi(a.substr(0, slash)); // stoi is more intuitive
-    denominator = std::stoi(a.substr(slash + 1));
-}
-
-std::string Fraction::to_string() {
-    std::string output = std::to_string(numerator) + "/" + std::to_string(denominator);
-    return output;
-}
-
 int gcd(int a, int b) {
     while (b != 0) {
         int temp = b;
@@ -32,4 +8,30 @@ int gcd(int a, int b) {
         a = temp;
         }
     return a;
+}
+
+Fraction::Fraction() {
+    denominator = 1;
+    numerator = 0;
+}
+
+
+Fraction::Fraction(int n,int d) {
+    int divisor = gcd(n,d);
+    denominator = d/divisor;
+    numerator = n/divisor;
+}
+
+Fraction::Fraction(std::string a) {
+    int slash = a.find('/');
+    numerator = std::stoi(a.substr(0, slash)); // stoi is more intuitive
+    denominator = std::stoi(a.substr(slash + 1));
+    int divisor = gcd(numerator,denominator);
+    numerator /= divisor;
+    denominator /= divisor;
+}
+
+std::string Fraction::to_string() {
+    std::string output = std::to_string(numerator) + "/" + std::to_string(denominator);
+    return output;
 }
