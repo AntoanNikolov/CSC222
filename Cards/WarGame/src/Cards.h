e99197b6025f4de84be8fcac66485964a415e848
AntoanNikolov <antoanameya@gmail.com>
2025-12-18 20:26:55 -0500

    add to header

--- Cards/WarGame/src/Cards.h
+++ Cards/WarGame/src/Cards.h
@@ -27,6 +27,8 @@ struct Card
     bool operator>=(const Card& c2) const;
     bool operator<=(const Card& c2) const;
     bool operator!=(const Card& c2) const;
+
+    Card merge_sort();
 };
 
 void swap_cards(Card* c1, Card* c2);
