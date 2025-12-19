08aaa1fe08bfc523045beb36daf890b1faf1cdcb
AntoanNikolov <antoanameya@gmail.com>
2025-12-18 20:17:53 -0500

    start merge sort test

--- Cards/WarGame/src/test_decks.cpp
+++ Cards/WarGame/src/test_decks.cpp
@@ -118,30 +118,22 @@ TEST_CASE("Test decks know their size") {
 
 
 
-// TEST_CASE("Test merge_sort") { // currently fails
-//     Deck d(0);
-//     d.add_card(Card(HEARTS, QUEEN));
-//     /*
-//     d.add_card(Card(SPADES, ACE));
-//     */
-//     d.add_card(Card(CLUBS, FOUR));
-//     /*
-//     d.add_card(Card(DIAMONDS, FOUR));
-//     d.add_card(Card(CLUBS, FIVE));
-//     d.add_card(Card(HEARTS, TEN));
-//     */
-//     d.add_card(Card(CLUBS, TWO));
-//     Deck d2 = d.merge_sort();
-//     CHECK(d2.cards[0].to_string() == "2 of Clubs");
-//     CHECK(d2.cards[1].to_string() == "4 of Clubs");
-//     CHECK(d2.cards[2].to_string() == "Queen of Hearts");
-//     /*
-//     CHECK(d2.cards[6].to_string() == "Ace of Spades");
-//     */
-//     // Check d is not changed.
-//     /*
-//     CHECK(d.cards[0].to_string() == "Queen of Hearts");
-//     CHECK(d.cards[3].to_string() == "4 of Diamonds");
-//     CHECK(d.cards[6].to_string() == "2 of Clubs");
-//     */
-// }
+TEST_CASE("Test merge_sort") {
+    Deck d(0);
+    d.add_card(Card(HEARTS, QUEEN));
+    d.add_card(Card(SPADES, ACE));
+    d.add_card(Card(CLUBS, FOUR));
+    d.add_card(Card(DIAMONDS, FOUR));
+    d.add_card(Card(CLUBS, FIVE));
+    d.add_card(Card(HEARTS, TEN));
+    d.add_card(Card(CLUBS, TWO));
+    Deck d2 = d.merge_sort();
+    CHECK(d2.cards[0].to_string() == "2 of Clubs");
+    CHECK(d2.cards[1].to_string() == "4 of Clubs");
+    CHECK(d2.cards[5].to_string() == "Queen of Hearts");
+    CHECK(d2.cards[6].to_string() == "Ace of Spades");
+    // Check d is not changed.
+    CHECK(d.cards[0].to_string() == "Queen of Hearts");
+    CHECK(d.cards[3].to_string() == "4 of Diamonds");
+    CHECK(d.cards[6].to_string() == "2 of Clubs");
+}
