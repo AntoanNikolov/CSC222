1832e6827bc035509abc2a4bb36bb8f6e300a49f
AntoanNikolov <antoanameya@gmail.com>
2025-12-19 09:36:16 -0500

    test cases for random

--- Cards/WarGame/src/test_random.cpp
+++ Cards/WarGame/src/test_random.cpp
@@ -1,4 +1,27 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
 #include "random.h"
-#include <iostream>
 #include <ctime>
-using namespace std;
+
+TEST_CASE("6-sided die rolls must be in valid range") {
+    srand(time(0));
+    for (int i = 0; i < 100; i++) {
+        int roll = random_between(1, 6);
+        CHECK(roll >= 1);
+        CHECK(roll <= 6);
+    }
+}
+
+TEST_CASE("reversed parameter order") {
+    srand(time(0));
+    for (int i = 0; i < 100; i++) {
+        int value = random_between(6, 3);
+        CHECK(value >= 3);
+        CHECK(value <= 6);
+    }
+}
+
+TEST_CASE("equal parameters") {
+    int value = random_between(5, 5);
+    CHECK(value == 5);
+}
